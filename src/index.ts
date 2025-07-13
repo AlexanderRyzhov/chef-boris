8ec1f0dbccae63c360d45c7f74877e7985f453ca
AlexanderRyzhov <58718193+AlexanderRyzhov@users.noreply.github.com>
2025-07-13 21:45:48 +0300

    feat: added structured response logic

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
-import  { mastra } from "./mastra/index.js";
 import 'dotenv/config';
+import { z } from "zod";
 
+import { mastra } from "./mastra/index.js";
 const chefBoris = mastra.getAgent("chefBoris");
 
 // Text response
@@ -13,15 +14,37 @@ const chefBoris = mastra.getAgent("chefBoris");
 
 // Streaming response
 
-const query = `Я нахожусь в гостях. У них в холодильнике есть курица, свинина, молоко,
-морковь, чеснок, сыр, яйца. Он еврей, а она татарка. Что мы можем приготовить на ужин под вино`;
+// const query = `Я нахожусь в гостях. У них в холодильнике есть курица, свинина, молоко,
+// морковь, чеснок, сыр, яйца. Он еврей, а она татарка. Что мы можем приготовить на ужин под вино`;
+// console.log(`Query: ${query}`);
+
+// const stream = await chefBoris.stream([{ role: "user", content: query }]);
+// console.log(`\n Шеф Борис: `);
+
+// for await (const chunk of stream.textStream) {
+//     process.stdout.write(chunk);
+// }
+
+// console.log("\n\n✅ Рецепт готов!");
+
+// Structured response
+
+const query = `Я хочу приготовить лазанью. Можешь составить рецепт лозаньи?`;
 console.log(`Query: ${query}`);
 
-const stream = await chefBoris.stream([{ role: "user", content: query }]);
-console.log(`\n Шеф Борис: `);
+const schema = z.object({
+    ingridients: z.array(
+        z.object({
+            name: z.string(),
+            amount: z.string(),
+            unit: z.string().optional(),
+        })
+    ),
+    steps: z.array(z.string()),
+});
 
-for await (const chunk of stream.textStream) {
-    process.stdout.write(chunk);
-}
-
-console.log("\n\n✅ Рецепт готов!");
+const response = await chefBoris.generate(
+    [{ role: "user", content: query }],
+    { output: schema }
+);
+console.log(`\n Шеф Борис: ${JSON.stringify(response.object, null, 2)}`);
